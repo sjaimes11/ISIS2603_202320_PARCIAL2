10cb62fb5488f3504faf8286b32004464f1ad419
Santiago Jaimes <sa.jaimesp@uniandes.edu.co>
2023-11-24 11:01:36 -0500

    curso detail

--- mynewapp/src/app/curso/curso-detail/curso-detail.component.ts
+++ mynewapp/src/app/curso/curso-detail/curso-detail.component.ts
@@ -1,28 +1,17 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Curso } from '../curso';
 
-import { CursoDetailComponent } from './curso-detail.component';
+@Component({
+  selector: 'app-curso-detail',
+  templateUrl: './curso-detail.component.html',
+  styleUrls: ['./curso-detail.component.css']
+})
+export class CursoDetailComponent implements OnInit {
 
-describe('CursoDetailComponent', () => {
-  let component: CursoDetailComponent;
-  let fixture: ComponentFixture<CursoDetailComponent>;
+  @Input() cursoDetail!: Curso;
+  constructor() { }
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CursoDetailComponent ]
-    })
-    .compileComponents();
-  }));
+  ngOnInit() {
+  }
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CursoDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+}
